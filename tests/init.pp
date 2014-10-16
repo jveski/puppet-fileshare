e7af8db9c69d127bebc384a16bedba664c1191bd
Jordan Olshevski <jordan@puppetlabs.com>
2014-10-16 15:00:54 -0700

    Remove unused attributes / contrain to Windows

--- tests/init.pp
+++ tests/init.pp
@@ -1,11 +1,16 @@
-file {'c:/test':
-  ensure => directory,
-}
+if $::osfamily == 'Windows' {
 
-win_fileshare {'temptest':
-  ensure      => present,
-  path        => 'c:/test',
-  comment     => 'test comment',
-  max_con     =>  0,
-  permissions =>  { 'WIN-0QPGSKB8HDD\vagrant' => 'full'}
+  file {'c:/test':
+    ensure => directory,
+  }
+
+  fileshare {'temptest':
+    ensure      => present,
+    path        => 'c:/test',
+    comment     => 'test comment',
+    max_con     =>  0,
+  }
+}
+else {
+  fail('Fileshare only supports Windows for the time being')
 }
